6d9907ea310c0a2ba944e2fdd3a57e14f191a0ac
Mohit Kumar Jaiswal <mohitkrjswl@gmail.com>
2024-09-03 16:52:50 +0530

    initial

---
+++ ai-interview/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>try not to click me</h2>
+      <Button>Click me</Button>
+    </div>
+  )
+};
